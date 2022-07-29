753bc8845d4ffccd737dfac5d33c3464ffb03c99
jimmyfloresmena <jimmy21andres@gmail.comm>
2022-07-29 10:55:44 -0600

    agregando estructura

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main>TikTOk clone</main>
+    </div>
+  );
+}
+
+export default App;
